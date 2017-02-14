d4200fcfc4f01f8f659601960ad4a128b23ca65a
Martin Jaime <martin.jaime-viveros@originate.com>
2017-02-14 14:41:54 -0800

    Adds socket.io for websockets handling

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 3000));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('frontend/build'));
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.send(fs.readFileSync('index.html', {encoding: 'utf8'}));
   console.log("responded to client")
 });
 
-app.listen(app.get('port'), function() {
+io.on('connection', (socket) => {
+  socket.emit("message", "hello world");
+  console.log("Client connected");
+});
+
+server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
